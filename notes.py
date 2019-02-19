fc05b92012ed6bcff0f95d6e2c833d8015db9e8c
henry <comprocho@gmail.com>
2019-02-18 22:28:19 -0500

    first commit

---
+++ notes.py
@@ -0,0 +1,3 @@
+#String Method
+	# find() - The find() method returns the lowest index of the substring if it is found in given string. If it is not found, it returns -1.
+		str.find(sub, start, end)
